5ec5940e5a8ea507d37bd6ad63bdeed2e0929835
taqiali973 <taqiali973@gmail.com>
2022-04-14 21:34:40 +0500

    last modified

--- pages/Hotel-Dashboard.js
+++ pages/Hotel-Dashboard.js
@@ -1,31 +1,28 @@
-import { Grid, Paper, Typography } from "@mui/material";
+import { Grid, Paper, Typography, TextField } from "@mui/material";
 import React, { useState } from "react";
 import LandingPage from "./Landing-Page";
 import AddIcon from "@mui/icons-material/Add";
 import Button from "@mui/material/Button";
-import Modal from "@mui/material/Modal";
-import Box from "@mui/material/Box";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import GuestList from "./Guest-List";
 
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 900,
-  height: 700,
-  bgColor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-};
 export default function HotelDashboard() {
-  // const [openModal, setOpenModal] = useState(false);
   const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
-    <div>
+    <>
       <LandingPage />
       <div
         style={{
@@ -37,7 +34,6 @@ export default function HotelDashboard() {
           container
           style={{
             width: "70%",
-
             margin: "0 auto",
             display: "flex",
             justifyContent: "center",
@@ -45,13 +41,15 @@ export default function HotelDashboard() {
         >
           <Grid item lg={10}>
             <Paper elevation={10} style={{ padding: "40px" }}>
-              <div style={{ display: "flex" }}>
+              <div style={{ display: "flex", marginTop: "20px" }}>
                 <div
                   style={{
                     width: "50%",
                   }}
                 >
-                  <Typography variant="h5">Hotel Information</Typography>
+                  <Typography variant="h5" sx={{ marginLeft: "10px" }}>
+                    Hotel Information
+                  </Typography>
                 </div>
                 <div
                   style={{
@@ -63,31 +61,11 @@ export default function HotelDashboard() {
                   <Button
                     variant="contained"
                     style={{ background: "grey" }}
-                    onClick={handleOpen}
+                    onClick={handleClickOpen}
                   >
                     <AddIcon style={{ marginRight: "5px" }} />
                     Add Guest
                   </Button>
-                  <Modal
-                    open={open}
-                    onClose={handleClose}
-                    aria-labelledby="modal-modal-title"
-                    aria-describedby="modal-modal-description"
-                  >
-                    <Box sx={style}>
-                      <Typography
-                        id="modal-modal-title"
-                        variant="h6"
-                        component="h2"
-                      >
-                        Text in a modal
-                      </Typography>
-                      <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                        Duis mollis, est non commodo luctus, nisi erat porttitor
-                        ligula.
-                      </Typography>
-                    </Box>
-                  </Modal>
                 </div>
               </div>
               {/* {openModal && <AddGuest closeModal={setOpenModal} />} */}
@@ -100,10 +78,10 @@ export default function HotelDashboard() {
                       padding: "10px",
                     }}
                   >
-                    <h3>Hotel Name :</h3>
-                    <h3>Phone :</h3>
-                    <h3>Verification Status :</h3>
-                    <h3>Total Rooms :</h3>
+                    <h3>Hotel Name : Pearl Continental</h3>
+                    <h3>Phone : 051-0988765</h3>
+                    <h3>Verification Status : verified</h3>
+                    <h3>Total Rooms : 100</h3>
                   </div>
                   <div
                     style={{
@@ -111,25 +89,98 @@ export default function HotelDashboard() {
                       padding: "20px",
                     }}
                   >
-                    <h3>Address:</h3>
-                    <h3>Police Station:</h3>
-                    <h3>Current Total Guest:</h3>
-                    <h3>Email:</h3>
+                    <h3>Address : Saddar Rawalpindi Cantt</h3>
+                    <h3>
+                      Police Station : Saddar Cannt Police Station Rawalpindi
+                    </h3>
+                    <h3>Current Total Guest : 60</h3>
+                    <h3>Email : pearlcontinental@gmail.com</h3>
                   </div>
                 </Grid>
-                <Grid item lg={12}>
+              </Grid>
+              <Grid container>
+                <Grid
+                  item
+                  lg={12}
+                  sx={{ borderTop: "2px solid black", padding: "20px" }}
+                >
                   <Typography variant="h5">Owner Information</Typography>
-                  <div style={{ marginTop: "20px" }}>
-                    <h3>Onwer Name:</h3>
-                    <h3>Onwer Cnic:</h3>
-                    <h3>Onwer Father:</h3>
+                  <div style={{ marginTop: "30px" }}>
+                    <h3>Onwer Name: Malik Jehangir Khan</h3>
+                    <h3>Onwer Cnic: 98765-9854315-9</h3>
+                    <h3>Onwer Father: Malik Aftab Khan </h3>
                   </div>
                 </Grid>
               </Grid>
             </Paper>
           </Grid>
         </Grid>
+        <div style={{ marginTop: "40px" }}>
+          <Grid container sx={{ width: "90%", margin: "0 auto" }}>
+            <Grid item lg={12}>
+              <GuestList />
+            </Grid>
+          </Grid>
+        </div>
       </div>
-    </div>
+
+      {/* Form Modal Start Here */}
+      <Dialog open={open} onClose={handleClose} maxWidth={"lg"}>
+        <DialogTitle
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Typography variant="h5">Add Guest</Typography>
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText style={{ marginTop: "20px" }}>
+            <form style={{ padding: "2%" }}>
+              <div>
+                <TextField
+                  id="outlined-basic"
+                  label="Name"
+                  variant="outlined"
+                  fullWidth
+                  sx={{ marginBottom: "14px" }}
+                />
+              </div>
+              <TextField
+                id="outlined-basic"
+                label="Cnic"
+                variant="outlined"
+                fullWidth
+                sx={{ marginBottom: "14px" }}
+              />
+
+              <TextField
+                id="outlined-basic"
+                label="Room No"
+                variant="outlined"
+                fullWidth
+                sx={{ marginBottom: "14px" }}
+              />
+
+              <TextField
+                id="outlined-basic"
+                label="Phone"
+                variant="outlined"
+                fullWidth
+              />
+            </form>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions sx={{ paddingRight: "30px" }}>
+          <Button onClick={handleClose} variant="contained">
+            Cancel
+          </Button>
+          <Button onClick={handleClose} variant="contained">
+            Add
+          </Button>
+        </DialogActions>
+      </Dialog>
+      {/* Form Modal End Here */}
+    </>
   );
 }
